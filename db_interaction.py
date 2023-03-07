936bf4cdba176fe097e0e3212f1f2038b772acce
AlekseyKibyakov <lexa111lexa6@mail.ru>
2023-03-07 13:02:28 +0300

    q

--- db_interaction.py
+++ db_interaction.py
@@ -3,9 +3,9 @@ from sqlalchemy.orm import sessionmaker
 from config import CONNSTR, DB_LOGIN
 from models import create_tables, User, Event
 
-DSN = f'postgresql://{DB_LOGIN["login"]}:\
-{DB_LOGIN["password"]}@{DB_LOGIN["host"]}:\
-{DB_LOGIN["port"]}/{DB_LOGIN["database"]}'
+# DSN = f'postgresql://{DB_LOGIN["login"]}:\
+# {DB_LOGIN["password"]}@{DB_LOGIN["host"]}:\
+# {DB_LOGIN["port"]}/{DB_LOGIN["database"]}'
 
 engine = sq.create_engine(CONNSTR)
 create_tables(engine)
